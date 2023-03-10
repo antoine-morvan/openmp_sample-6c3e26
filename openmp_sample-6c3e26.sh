124aa6c977293b81f4df0502a5c1c8249647d5f1
Antoine Morvan <antoine.morvan.pro@gmail.com>
2023-03-10 12:18:10 +0100

    Create openmp_sample-6c3e26.sh

---
+++ openmp_sample-6c3e26.sh
@@ -0,0 +1,4 @@
+# Crash reproducer for clang version 15.0.7 (git@gitlab.bench.local:bmorvana/software.git 33e2324d8e7450b0f7ea6c4e4a5deffa3b14e073)
+# Driver args: "-flang-experimental-exec" "-fopenmp" "-O3" "-march=native" "-mtune=native" "openmp_sample.F90"
+# Original command:  "/home_nfs/bmorvana/software/Linux/x86_64/default/llvm-15.0.7/prefix/bin/flang-new" "-fc1" "-triple" "x86_64-unknown-linux-gnu" "-emit-obj" "-fopenmp" "-fcolor-diagnostics" "-O3" "-o" "/dev/shm/openmp_sample-f2f252.o" "-x" "f95-cpp-input" "openmp_sample.F90"
+ "/home_nfs/bmorvana/software/Linux/x86_64/default/llvm-15.0.7/prefix/bin/flang-new" "-fc1" "-triple" "x86_64-unknown-linux-gnu" "-emit-obj" "-fopenmp" "-fcolor-diagnostics" "-O3" "-x" "f95-cpp-input" "openmp_sample-6c3e26"
